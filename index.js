88ce5796c5bea1825caa8b504f5942e51794a497
Nick de Jong <aries1542@Large-Lion>
2024-11-15 23:09:58 -0700

    collision detection

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const wss = new WebSocket.WebSocketServer({ server: server })
 
 env = new environment.Environment()
 env.rectObstacles.push(environment.makeCrate(200, 250))
-env.rectObstacles.push(environment.makeHorizontalWall(200, 150, 75))
-env.rectObstacles.push(environment.makeVerticalWall(200, 150, 200))
+// env.rectObstacles.push(environment.makeHorizontalWall(300, 250, 75))
+// env.rectObstacles.push(environment.makeVerticalWall(300, 250, 200))
 
 
 numClients = 0;
@@ -48,18 +48,55 @@ wss.on('connection', function (ws) {
 function movePlayer(playerID, deltaX, deltaY, rotation) {
     player = env.players[playerID]
     player.rotation = rotation
-    player.x += deltaX
-    player.y += deltaY
-    // if (!positionAvailable(new_x, new_y)) {
-
-    // }
+    let newX = player.x + deltaX
+    let newY = player.y + deltaY
+    let [fixedX, fixedY] = fixPosition(newX, newY)
+    player.x = fixedX
+    player.y = fixedY
 }
 
-// function positionAvailable(x, y) {
-//     radius = 25  // Client-side radius of player
-//     for (var object of env.rectObstacles) {
-//         left = object.x
-//         right = object.x + object.width
-//         if (x-25 > 3)
-//     }
-// }
+function fixPosition(x, y) {
+    radius = 25  // Client-side radius of player
+    
+    var xModified = false
+    var yModified = false
+    
+    let playerLeft = x - radius
+    let playerRight = x + radius
+    
+    let playerTop = y - radius
+    let playerBottom = y + radius
+
+    for (var object of env.rectObstacles) {
+        let wallLeft = object.x - object.width/2
+        let wallRight = object.x + object.width/2
+        let wallTop = object.y - object.height/2
+        let wallBottom = object.y + object.height/2
+        
+        if ((!xModified && (playerTop < wallBottom-10 && playerBottom > wallTop+10) && !(playerLeft >= wallRight || playerRight <= wallLeft))) {
+            // console.log("its happening (x)")
+            if (playerLeft < wallRight && playerRight > wallRight) { // left side of player is clipping wall
+                x = wallRight+radius
+                xModified = true
+            } else {
+                x = wallLeft-radius
+                xModified = true
+            }
+        }
+        
+        if ((!yModified && (playerLeft < wallRight-10 && playerRight > wallLeft+10) && !(playerTop >= wallBottom || playerBottom <= wallTop))) {
+            // console.log("its happening (y)")
+            if (playerTop < wallBottom && playerBottom > wallBottom) { // top side of player is clipping wall
+                y = wallBottom+radius
+                yModified = true
+            } else {
+                y = wallTop-radius
+                yModified = true
+            }
+        }
+
+        if (xModified && yModified) {break}
+    }
+
+    return [x, y]
+}
