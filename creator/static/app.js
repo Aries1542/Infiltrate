b8fdbb076d5a251fe9c2f88bd813dd1412a5a48e
aries1542 <ndejongster@gmail.com>
2025-03-16 17:23:19 -0600

    Creator runs as its own server program; Saves and loads data to file

--- creator/static/app.js
+++ creator/static/app.js
@@ -9,6 +9,8 @@ two.appendTo(document.body)
 const centerX = .5 * two.width;
 const centerY = .5 * two.height;
 
+const filename = "mapData";
+
 const game = {
     gridSize: 20,
     grid: null,
@@ -16,15 +18,16 @@ const game = {
     items: null,
     nextId: 1,
     moveSpeed: 2,
-    offset: {x: 0, y: 0},
+    offset: { x: 0, y: 0 },
 }
 
 const main = () => {
     game.grid = drawGrid();
     game.obstacles = two.makeGroup();
     game.items = two.makeGroup();
+    loadData();
     drawPlayer(.5 * two.width, .5 * two.height, 0, "spawnReference");
-    console.log("Options:\n\tV: make obstacle\n\tC: make coin\n\tR: delete\n\tP: print JSON data");
+    console.log("Options:\n\tV: make obstacle\n\tC: make coin\n\tR: delete\n\tP: save date");
     console.log("Current mode: make obstacle");
     setInterval(update, 15);
 };
@@ -38,7 +41,7 @@ const update = () => {
 }
 
 const getKeyInput = () => {
-    const delta = {x: 0, y: 0};
+    const delta = { x: 0, y: 0 };
     if (keysDown["KeyW"]) {
         delta.y -= 1;
     }
@@ -58,7 +61,7 @@ const getKeyInput = () => {
     return delta;
 }
 
-window.addEventListener("resize", function(){
+window.addEventListener("resize", function () {
 });
 
 const keysDown = {
@@ -67,8 +70,8 @@ const keysDown = {
     "KeyS": false,
     "KeyD": false,
 };
-onkeydown = onkeyup = (event) => {
-    keysDown[event.code] = (event.type === "keydown");
+onkeydown = (event) => {
+    keysDown[event.code] = true;
     switch (event.code) {
         case "KeyW":
         case "KeyA":
@@ -96,13 +99,16 @@ onkeydown = onkeyup = (event) => {
             break;
     }
 };
+onkeyup = (event) => {
+    keysDown[event.code] = false;
+};
 let mode = "makeObstacle";
 
 let startX = null;
 let startY = null;
 let preview = null;
 onmousedown = (event) => {
-    event = {x: event.x, y: event.y};
+    event = { x: event.x, y: event.y };
     console.log("Event: ", event);
     console.log("Before: ", event.x, event.y);
     console.log("Offset: ", game.offset.x, game.offset.y);
@@ -124,7 +130,7 @@ onmousedown = (event) => {
     }
 };
 onmousemove = (event) => {
-    event = {x: event.x, y: event.y};
+    event = { x: event.x, y: event.y };
     event.x += game.offset.x;
     event.y += game.offset.y;
     switch (mode) {
@@ -141,7 +147,7 @@ onmousemove = (event) => {
     }
 }
 onmouseup = (event) => {
-    event = {x: event.x, y: event.y};
+    event = { x: event.x, y: event.y };
     event.x += game.offset.x;
     event.y += game.offset.y;
     switch (mode) {
@@ -179,10 +185,10 @@ const makeObstaclePreview = (event) => {
         return;
     }
     if (preview) preview.remove()
-    let left = startX - (startX % game.gridSize) - game.gridSize/2;
-    let top = startY - (startY % game.gridSize) - game.gridSize/2;
-    let right = event.x - (event.x % game.gridSize) + game.gridSize/2;
-    let bottom = event.y - (event.y % game.gridSize) + game.gridSize/2;
+    let left = startX - (startX % game.gridSize) - game.gridSize / 2;
+    let top = startY - (startY % game.gridSize) - game.gridSize / 2;
+    let right = event.x - (event.x % game.gridSize) + game.gridSize / 2;
+    let bottom = event.y - (event.y % game.gridSize) + game.gridSize / 2;
     if (right < left) {
         const temp = left;
         left = right;
@@ -221,10 +227,10 @@ const deletePreview = (event) => {
         return;
     }
     if (preview) preview.remove()
-    let left = startX - (startX % game.gridSize) - game.gridSize/2;
-    let top = startY - (startY % game.gridSize) - game.gridSize/2;
-    let right = event.x - (event.x % game.gridSize) + game.gridSize/2;
-    let bottom = event.y - (event.y % game.gridSize) + game.gridSize/2;
+    let left = startX - (startX % game.gridSize) - game.gridSize / 2;
+    let top = startY - (startY % game.gridSize) - game.gridSize / 2;
+    let right = event.x - (event.x % game.gridSize) + game.gridSize / 2;
+    let bottom = event.y - (event.y % game.gridSize) + game.gridSize / 2;
     if (right < left) {
         const temp = left;
         left = right;
@@ -243,19 +249,19 @@ const deletePreview = (event) => {
         color: "#a008",
     });
     for (const obstacle of game.obstacles.children) {
-        if (obstacle.position.x-obstacle.width/2 > preview.position.x+preview.width/2 ||
-            obstacle.position.x+obstacle.width/2 < preview.position.x-preview.width/2 ||
-            obstacle.position.y-obstacle.height/2 > preview.position.y+preview.height/2 ||
-            obstacle.position.y+obstacle.height/2 < preview.position.y-preview.height/2
+        if (obstacle.position.x - obstacle.width / 2 > preview.position.x + preview.width / 2 ||
+            obstacle.position.x + obstacle.width / 2 < preview.position.x - preview.width / 2 ||
+            obstacle.position.y - obstacle.height / 2 > preview.position.y + preview.height / 2 ||
+            obstacle.position.y + obstacle.height / 2 < preview.position.y - preview.height / 2
         ) obstacle.fill = "#008"
         else obstacle.fill = "#a00"
     }
     for (const item of game.items.children) {
         if (item.type === "coin" && (
-            item.position.x-item.radius > preview.position.x+preview.width/2 ||
-            item.position.x+item.radius < preview.position.x-preview.width/2 ||
-            item.position.y-item.radius > preview.position.y+preview.height/2 ||
-            item.position.y+item.radius < preview.position.y-preview.height/2)
+            item.position.x - item.radius > preview.position.x + preview.width / 2 ||
+            item.position.x + item.radius < preview.position.x - preview.width / 2 ||
+            item.position.y - item.radius > preview.position.y + preview.height / 2 ||
+            item.position.y + item.radius < preview.position.y - preview.height / 2)
         ) item.fill = "#fe7";
         else item.fill = "#a00";
     }
@@ -265,19 +271,19 @@ const deleteComplete = (event) => {
         return;
     }
     for (const obstacle of game.obstacles.children) {
-        if (obstacle.position.x-obstacle.width/2 > preview.position.x+preview.width/2 ||
-            obstacle.position.x+obstacle.width/2 < preview.position.x-preview.width/2 ||
-            obstacle.position.y-obstacle.height/2 > preview.position.y+preview.height/2 ||
-            obstacle.position.y+obstacle.height/2 < preview.position.y-preview.height/2
+        if (obstacle.position.x - obstacle.width / 2 > preview.position.x + preview.width / 2 ||
+            obstacle.position.x + obstacle.width / 2 < preview.position.x - preview.width / 2 ||
+            obstacle.position.y - obstacle.height / 2 > preview.position.y + preview.height / 2 ||
+            obstacle.position.y + obstacle.height / 2 < preview.position.y - preview.height / 2
         ) obstacle.fill = "#008";
         else obstacle.remove();
     }
     for (const item of game.items.children) {
         if (item.type === "coin" && (
-            item.position.x-item.radius > preview.position.x+preview.width/2 ||
-            item.position.x+item.radius < preview.position.x-preview.width/2 ||
-            item.position.y-item.radius > preview.position.y+preview.height/2 ||
-            item.position.y+item.radius < preview.position.y-preview.height/2)
+            item.position.x - item.radius > preview.position.x + preview.width / 2 ||
+            item.position.x + item.radius < preview.position.x - preview.width / 2 ||
+            item.position.y - item.radius > preview.position.y + preview.height / 2 ||
+            item.position.y + item.radius < preview.position.y - preview.height / 2)
         ) item.fill = "#fe7";
         else item.remove();
     }
@@ -287,12 +293,12 @@ const deleteComplete = (event) => {
     preview = null;
 }
 
-saveData = () => {
+const saveData = () => {
     const obstacles = []
     for (const obstacle of game.obstacles.children) {
         obstacles.push({
-            x: obstacle.position.x - obstacle.width/2,
-            y: obstacle.position.y - obstacle.height/2,
+            x: obstacle.position.x - obstacle.width / 2,
+            y: obstacle.position.y - obstacle.height / 2,
             width: obstacle.width,
             height: obstacle.height,
             color: obstacle.fill,
@@ -309,13 +315,39 @@ saveData = () => {
         });
     }
     localToGlobalCoords(items)
-    console.log(JSON.stringify({obstacles, items}));
+    console.log("Sent to server: ", JSON.stringify({ obstacles, items }));
+
+    fetch("/save", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ filename: filename, data: JSON.stringify({ obstacles, items }) }),
+    })
+
+};
+
+const loadData = () => {
+    fetch("/load?filename=" + filename)
+        .then(response => response.json())
+        .then(data => {
+            const { obstacles, items } = data;
+            two.scene.position.set(0, 0);
+            drawMap(obstacles, items);
+        });
 };
 
 const localToGlobalCoords = (data) => {
     for (const datum of data) {
         datum.x = (datum.x - centerX);
         datum.y = (datum.y - centerY);
+    }
+};
+
+const globalToLocalCoords = (data) => {
+    for (const datum of data) {
+        datum.x = (datum.x + centerX);
+        datum.y = (datum.y + centerY);
     }
 };
 
